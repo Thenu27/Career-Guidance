6b4c46570b9fc7ed9695f964c58edbbed755f430
Thenu27 <thenuliyana@gmail.com>
2025-01-17 15:18:51 +0530

    Connection testing

--- server/src/server.js
+++ server/src/server.js
@@ -133,7 +133,15 @@ app.use((req, res, next) => {
 });
 
 
-
+db.raw('SELECT 1')
+    .then(() => {
+        console.log('Database connection established successfully!');
+        process.exit(0); // Exit the process after the test
+    })
+    .catch((err) => {
+        console.error('Database connection failed:', err.message);
+        process.exit(1); // Exit with error code
+    });
 
 app.use(express.json());
 
