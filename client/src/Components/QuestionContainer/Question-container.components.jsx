cd031324b213b17ebc65fd04f845eef2608e926c
Thenu27 <thenuliyana@gmail.com>
2025-01-21 21:30:50 +0530

    Removed proxy

--- client/src/Components/QuestionContainer/Question-container.components.jsx
+++ client/src/Components/QuestionContainer/Question-container.components.jsx
@@ -72,7 +72,10 @@ const QuestionContainer = () =>{
           setquestionAndAnswers(updatedAnswers)
     },[questions])
 
+    console.log("REACT_APP_API_URL:", process.env.REACT_APP_API_URL);
+    console.log("Environment Variables:", process.env);
 
+    
 
 
 
